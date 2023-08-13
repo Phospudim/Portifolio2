2e3f5f75f1ad67c17a66a0fa640436823b6c6b5b
Alexsandro Gomes dias <126702081+Phospudim@users.noreply.github.com>
2023-08-13 10:29:12 -0300

    update 3

---
+++ my-blog/pages/components/Circulo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Circulo() {
+
+    return(
+        <button className={`bg-blue-500 text-white p-4 rounded-md`}>Botão
+
+        
+        </button>
+    )
+}
--- my-blog/pages/components/icon-tech.tsx
+++ my-blog/pages/components/icon-tech.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 
-
-
 export default function iconTech() {
   const meuEstilo = {
     
